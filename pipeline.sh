d2ca8e049c14d0f9976ea5e3b074230714a2ca0d
Sae Kaneko <sakaneko@ucsd.edu>
2014-10-21 11:20:48 -0700

    added bash script

---
+++ pipeline.sh
@@ -0,0 +1,23 @@
+echo "Cleaning up files before downloading..." #display what we're doing
+
+rm *.txt #clean up before downloading files 
+rm *.tmp
+rm *.csv
+
+
+echo downloading data... #quotation marks are not necessary
+curl https://github.com/zonca/swcarpentry-workshop-pandas/blob/master/rawdata/rawdata.zip?raw=true -o rawdata.zip -L
+
+echo unzipping data...
+unzip rawdata.zip
+
+rm rawdata.zip #dont need this anymore
+rm *.tmp #these files are useless
+
+for f in *.txt
+do
+	mv $f ${f/txt/csv} #indentation is not necessary but helps easy reading
+done
+
+echo Available files
+ls *.csv
